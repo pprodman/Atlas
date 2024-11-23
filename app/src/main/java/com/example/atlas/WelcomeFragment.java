2dcf39b492306aa6aba7cdb70052fe91cbbb66ef
ProDMaN <pprodman@gmail.com>
2024-11-23 20:36:00 +0100

    Initial commit - v1.0 Organizacion de navegación

---
+++ app/src/main/java/com/example/atlas/WelcomeFragment.java
@@ -0,0 +1,41 @@
+package com.example.atlas;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.atlas.databinding.FragmentWelcomeBinding;
+
+public class WelcomeFragment extends Fragment {
+
+    FragmentWelcomeBinding binding;
+    NavController navController;
+
+    // Inicializa el fragmento
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+        binding.btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_welcomeFragment_to_homeFragment);
+            };
+        });
+    };
+}
